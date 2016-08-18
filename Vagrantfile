a5d970cebf89b40fda632f3f746b064e918f01fa
LeeDr <lee.drengenberg@elastic.co>
2016-08-17 20:44:25 -0500

    Running tests from host, not on VM

--- Vagrantfile
+++ Vagrantfile
@@ -3,40 +3,17 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
 
-#  config.vm.box = "boxcutter/ubuntu1604-desktop"
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.box_url = 'https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/14.04/providers/virtualbox.box'
-#  config.vm.box = "elastic/ubuntu-16.04-x86_64"
-  
-  
-  
-#  config.vm.network :forwarded_port, guest:4444, host:4444
+  config.vm.box = "elastic/ubuntu-14.04-x86_64"
+
   config.vm.network :forwarded_port, guest:5601, host:5620
   config.vm.network :forwarded_port, guest:9200, host:9220
-#  config.vm.network :private_network, ip: "192.168.33.10"
 
    config.vm.provision "shell", path: "qa/integration5.0snapshot.sh"
 
   config.vm.provider :virtualbox do |vb|
     vb.memory = 6144
     vb.cpus = 4
-    #vb.gui = true
-    #vb.customize ["modifyvm", :id, "--accelerate3d", "off"]
     vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate//vagrant","1"]
   end
-  
-#  config.vm.synced_folder "..\\test\\", "/test", create: true
-#  config.vm.synced_folder "./", "/vagrant", create: true
-  
-#  config.vm.provision :shell do |sh|
-#    sh.path = config.bootstrap.privileged
-#    sh.privileged = true
-#  end
 
-#  config.vm.provision :shell do |sh|
-#    sh.path = config.bootstrap.non_privileged
-#    sh.privileged = false
-#  end
-  
 end
-
--- qa/integration5.0snapshot.sh
+++ qa/integration5.0snapshot.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-#cd "$(dirname "$0")"
 QADIR=/vagrant/qa/
 cd $QADIR
 
-#if [ "$(id -u)" != "0" ]; then
-#   echo "This script must be run as root" 1>&2
-#   exit 1
-#fi
 date
-echo sleep 60
-sleep 60
-date
-
 
 if [ `grep DISTRIB_ID /etc/*-release | cut -d= -f2` != 'Ubuntu' ]; then
   echo "This script was written for Ubuntu (apt-get, dpkg, etc)"
   exit 1
 fi
-
-sleep 30
 
 java_version=$(java -version 2>&1 | grep version | sed 's|.* version "\(.*\..*\)\..*_.*"|\1|')
 ubuntu_version=$(grep VERSION_ID /etc/*-release)
@@ -28,30 +17,9 @@ if [[ $java_version < 1.8 ]]; then
   sudo apt-get update
   echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo /usr/bin/debconf-set-selections
   sudo apt-get install -y oracle-java8-installer
-  #sudo apt-get update
-  #sudo apt-get install -y openjdk-8-jre-headless
-  #sudo apt-get install -f
 fi
 
 java -version
-# node 4.4.7
-# curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-
-# Chrome browser
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-#sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-#sudo apt-get install -y git google-chrome-stable
-
-# sudo apt-get install -y git
-
-# install chrome browser
-# sudo apt-get install -y libxss1 libappindicator1 libindicator7
-# wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome*.deb
-
-
-#sudo startx
 
 . ./setenv.sh
 
@@ -62,63 +30,67 @@ PACKAGE=deb
 PLATFORM=-amd64
 #./cleanup.sh
 
-
+echo "-- Get the packages"
 ls packetbeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE} || wget -q http://${BASEURL}/download/beats/packetbeat/packetbeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}
 ls filebeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}   || wget -q http://${BASEURL}/download/beats/filebeat/filebeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}
 ls metricbeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE} || wget -q http://${BASEURL}/download/beats/metricbeat/metricbeat-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}
 ls kibana-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}     || wget -q http://${BASEURL}/download/kibana/kibana-${VERSION}${SNAPSHOT}${PLATFORM}.${PACKAGE}
-# http://snapshots.elastic.co/download/kibana/kibana-5.0.0-alpha5-SNAPSHOT-amd64.deb
 
 ls logstash-${VERSION}${SNAPSHOT}.${PACKAGE}      || wget -q http://${BASEURL}/download/logstash/logstash-${VERSION}${SNAPSHOT}.${PACKAGE}
 ls elasticsearch-${VERSION}${SNAPSHOT}.${PACKAGE} || wget -q http://${BASEURL}/download/elasticsearch/elasticsearch-${VERSION}${SNAPSHOT}.${PACKAGE}
 
+echo "-- Install packages"
 ./install_packages.sh || exit 1
 
 # Install Platinum License?
 
-echo Configure beats authenication
+echo "-- Configure beats authenication"
 ./configure_beats.sh || exit 1
 
-echo Install Elasticsearch X-Pack
+echo "-- Install Elasticsearch X-Pack"
 ls x-pack-${VERSION}${SNAPSHOT}.zip || wget -q http://${BASEURL}/download/elasticsearch/plugins/x-pack/x-pack-${VERSION}${SNAPSHOT}.zip
 time sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b file:///${QADIR}/x-pack-${VERSION}${SNAPSHOT}.zip
 
-echo Install Kibana UI Plugins
+echo "-- Install Kibana UI Plugins"
 time sudo /usr/share/kibana/bin/kibana-plugin install https://${BASEURL}/download/kibana/plugins/x-pack/x-pack-${VERSION}${SNAPSHOT}.zip
 
 echo "-- Configure Shield users/roles for Kibana"
 #/usr/share/elasticsearch/bin/x-pack/users useradd $KIBANAFILEUSER -r kibanaUser -p $KIBANAFILEPWD || exit 1
 # create user for logstash to connect to Elasticsearch (used in logstash.conf
 /usr/share/elasticsearch/bin/x-pack/users useradd $LOGSTASHUSER -r logstash -p $LOGSTASHPWD || exit 1
-# let logstash process read syslog
-sudo setfacl -m u:logstash:r /var/log/syslog || exit 1
+
+echo "-- let logstash process read syslog"
+#sudo setfacl -m u:logstash:r /var/log/syslog || exit 1 (not mounted with acls)
+
+sudo chmod o+r /var/log/syslog
 sudo cp logstash.conf /etc/logstash/conf.d/ || exit 1
 
 # curl put watcher config
 
 # curl put watcher trigger data?
 
-# Set network.host for Elasticsearch so we can access it outside the vagrant machine
+echo "-- Set network.host for Elasticsearch so we can access it outside the vagrant machine"
 echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 # Now that we set host, we have to make it more production like
 echo "discovery.zen.minimum_master_nodes: 0" >> /etc/elasticsearch/elasticsearch.yml
 
-# and also set jvm min heap size
+echo "-- set jvm min heap size"
 sed -i 's/-Xms256m/-Xms2g/' /etc/elasticsearch/jvm.options
 
 
-
+echo "-- Start services"
 ./start_services.sh || exit 1
 
 # make some packet data
 ping -c 100 www.google.com >/dev/null &
 
-
+echo "-- Wait for Elasticsearch and Kibana to be ready"
 ./check.sh
 
+echo "-- Create a Kibana user (iron man)"
 ./create_kibana_user.sh
 
-
+echo "-- Load Beats index patterns, saves searches, visualizations, and dashboards"
 pushd /usr/share/filebeat/kibana/
 ./import_dashboards.sh -u $ELASTICUSER:$ELASTICPWD &>/dev/null
 popd
@@ -130,3 +102,5 @@ popd
 pushd /usr/share/metricbeat/kibana/
 ./import_dashboards.sh -u $ELASTICUSER:$ELASTICPWD &>/dev/null
 popd
+
+#./run_tests.sh
