76439b6dd1d193e78b610e36dfeb0c73550a5be4
LeeDr <lee.drengenberg@elastic.co>
2016-09-01 09:46:13 -0500

    Improve check script logging output

--- qa/check.sh
+++ qa/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+   echo -e "\n\nERROR: This script must be run as root\n\n"
    exit 1
 fi
 
 if [ `grep DISTRIB_ID /etc/*-release | cut -d= -f2` != 'Ubuntu' ]; then
-  echo "This script was written for Ubuntu (apt-get, dpkg, etc)"
+  echo -e "\n\nERROR: This script was written for Ubuntu (apt-get, dpkg, etc)\n\n"
   exit 1
 fi
 
@@ -20,24 +20,25 @@ for i in $PRODUCTS; do service $i status; done
 
 rm index.html*
 # try 10 times, 2 seconds apart
-echo -e "\n `date`-----------------Elasticserach----------------------------------------"
+echo -e "\n `date`------------wait for Elasticserach to be up----------------------------------------"
 for i in `seq 1 20`; do echo "wget es index" && sleep 4 && wget -q --http-user=$ELASTICUSER --http-password=$ELASTICPWD http://localhost:9200 && break; done
 cat index.html
 
-echo -e "\n `date`-----------------Kibana log-------------------------------------------"
-# try 10 times, 2 seconds apart
-for i in `seq 1 20`; do echo "${i} `date` grep kibana log for 'green'" && sleep 4 & grep '"plugin:elasticsearch@.*","info"],"pid":.*,"state":"green"' /var/log/kibana/kibana.stdout && break; done
+echo -e "\n `date`----------- Wait for elasticsearch and kibana plugin status to be green ----------------"
+for i in `seq 1 30`; do echo "${i} `date` grep kibana log for 'green'" && sleep 4 && grep '"plugin:elasticsearch@.*","info"],"pid":.*,"state":"green"' /var/log/kibana/kibana.stdout && break; done
+for i in `seq 1 30`; do echo "${i} `date` grep kibana log for 'green'" && sleep 4 && grep '"plugin:kibana@.*","info"],"pid":.*,"state":"green"' /var/log/kibana/kibana.stdout && break; done
 #for i in `seq 1 20`; do echo "grep kibana log for 'green'" && sleep 4 &&  journalctl -u kibana.service --since "5 minutes ago"| grep "plugin:elasticsearch.*green" && break; done
 
 
-for plugin in console kbn_doc_views kbn_vislib_vis_types markdown_vis metric_vis spy_modes status_page table_vis xpack_main elasticsearch graph security reporting monitoring; do (
-  grep "plugin:$plugin" /var/log/kibana/kibana.stdout | tail -n1 | sed 's|.*plugin:\(.*\)","info".*"state":"\([^"]*\).*|\2  \1|'
+echo -e "\n `date`----------- Status of each kibana plugin ------------------------"
+for plugin in kibana console xpack_main elasticsearch graph security reporting monitoring; do (
+  echo -e "`grep "plugin:$plugin" /var/log/kibana/kibana.stdout | tail -n1 | sed 's|.*plugin:\(.*\)","info".*"state":"\([^"]*\).*|\2|'` ${plugin}"
 )
 done
 
 #journalctl --since "2016-03-30"
 #journalctl -u kibana.service
-echo -e "\n----------------------------------------------------------------------"
+#echo -e "\n----------------------------------------------------------------------"
 #/usr/share/elasticsearch/bin/xpack/esusers useradd admin -r admin -p notsecure
 
 echo -e "\n `date`-----------------Shield File Users-----------------------------------------"
@@ -47,5 +48,5 @@ curl -s -XGET http://$ELASTICUSER:$ELASTICPWD@127.0.0.1:9200/_xpack/security/use
 echo -e "\n `date`-----------------Shield Native Roles-----------------------------------------"
 curl -s -XGET http://$ELASTICUSER:$ELASTICPWD@127.0.0.1:9200/_xpack/security/role?pretty | grep ".*\".*{" | grep -v metadata | sed 's/: {//'
 
-echo " `date`-- http://elastic:changeme@localhost:9200/_cat/indices"
+echo -e "\n `date`-- http://elastic:changeme@localhost:9200/_cat/indices"
 curl -s http://elastic:changeme@localhost:9200/_cat/indices
