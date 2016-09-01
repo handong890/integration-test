abafbfb61db1bf0b438a98288899984e5cb9fd9a
LeeDr <lee.drengenberg@elastic.co>
2016-09-01 09:47:15 -0500

    Cut defaultTryTimeout from 40 to 20 seconds, nothing takes this long

--- test/intern.js
+++ test/intern.js
@@ -16,7 +16,7 @@ define(function (require) {
     excludeInstrumentation: /.*/,
 
     defaultTimeout: 90000,
-    defaultTryTimeout: 40000, // tryForTime could include multiple 'find timeouts'
+    defaultTryTimeout: 20000, // tryForTime could include multiple 'find timeouts'
     defaultFindTimeout: 10000  // this is how long we try to find elements on page
   }, serverConfig);
 });
