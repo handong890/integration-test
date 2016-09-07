75f9cbe37feef0b81834007740a0f9e2b6f315a8
LeeDr <lee.drengenberg@elastic.co>
2016-09-07 11:31:06 -0500

    Fixes issue #3 and uses a packetbeat vis for the packetbeat test

--- test/functional/apps/packetbeat/_packetbeat.js
+++ test/functional/apps/packetbeat/_packetbeat.js
@@ -15,17 +15,17 @@ bdd.describe('check packetbeat', function describeGraphTests() {
     return PageObjects.common.navigateToApp('visualize');
   });
 
-  bdd.it('In vs Out Network Bytes - should contain at least 3 data points', function () {
-    return PageObjects.visualize.openSavedVisualization('In vs Out Network Bytes')
+  bdd.it('Latency histogram - should contain at least 3 data points', function () {
+    return PageObjects.visualize.openSavedVisualization('Latency histogram')
     .then(() => {
       return PageObjects.common.sleep(3000)
     })
     .then(() => {
-      return PageObjects.visualize.getLineChartData('data-label="In Bytes"');
+      return PageObjects.visualize.getLineChartData('data-label="Count"');
     })
     .then(function showData(data) {
-      PageObjects.common.saveScreenshot('In_vs_Out_Network_Bytes');
-      PageObjects.common.debug('Found ' + data.length + ' data points for "In Bytes"');
+      PageObjects.common.saveScreenshot('Latency_histogram');
+      PageObjects.common.debug('Found ' + data.length + ' data points for "Latency histogram"');
       expect(data.length > 2).to.be(true);
     });
   });
