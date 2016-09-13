a9446ea3f516c195bf40dbf39aa9242b25161168
LeeDr <lee.drengenberg@elastic.co>
2016-09-13 17:11:53 -0500

    Rename logstash role to logstash_writer, loggy user to logstash_internal, add monitor cluster priv.

--- qa/create_logstash_role_user.sh
+++ qa/create_logstash_role_user.sh
@@ -3,8 +3,8 @@ if [ -z "$NATIVEKIBANAUSER" ]; then . ./setenv.sh; fi
 
 echo "-- Create Logstash role"
 
-curl -s -POST http://${ELASTICUSER}:${ELASTICPWD}@localhost:9200/_xpack/security/role/logstash -d '{
-  "cluster": ["manage_index_templates"],
+curl -s -POST http://${ELASTICUSER}:${ELASTICPWD}@localhost:9200/_xpack/security/role/logstash_writer -d '{
+  "cluster": ["manage_index_templates","monitor"],
   "indices": [
     {
       "names": [ "logstash-*" ],
@@ -13,10 +13,10 @@ curl -s -POST http://${ELASTICUSER}:${ELASTICPWD}@localhost:9200/_xpack/security
   ]
 }'
 
-echo "-- Create Logstash user"
-curl -s -POST http://${ELASTICUSER}:${ELASTICPWD}@localhost:9200/_xpack/security/user/${LOGSTASHUSER} -d '{
+echo "-- Create logstash_internal user"
+curl -s -POST http://${ELASTICUSER}:${ELASTICPWD}@localhost:9200/_xpack/security/user/logstash_internal -d '{
   "password" : "changeme",
-  "roles" : [ "logstash" ],
-  "full_name" : "loggy",
-  "email" : "loggy@test.co"
+  "roles" : [ "logstash_writer" ],
+  "full_name" : "logstash_internal",
+  "email" : "logstash_internal@test.co"
 }'
